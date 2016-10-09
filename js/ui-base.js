d77718e849034933620e3460003db0c8e51e70d9
thegi <deokim77@gmail.com>
2016-10-09 22:56:26 +0900

    dataset polyfill 추가, tabs 수정, modal 수정

--- js/ui-base.js
+++ js/ui-base.js
@@ -1,5 +1,33 @@
 'use strict';
 
+/* dataset */
+var dataset = {
+	get: function( el, attr ){
+		return el.dataset[attr];
+	},
+	set : function( el, attr, val ){
+		el.dataset[attr] = val;
+	},
+	del: function ( el, attr) {
+		delete el.dataset[attr];
+	}
+};
+// dataset polyfill
+var hasDataset = 'dataset' in document.body;
+if(!hasDataset) {
+	dataset = {
+		get : function( el, attr ){
+			return el.getAttribute('data-' + attr);
+		},
+		set : function( el, attr, val ){
+			el.setAttribute('data-' + attr, val)
+		},
+		del: function ( el, attr) {
+			el.removeAttribute('data-' + attr);
+		}
+	};
+};
+
 /* ToggleAttirbute */
 var toggleAttirbute = function(attribute, el){
 	if( el.getAttribute(attribute) == 'true' ){
@@ -7,7 +35,6 @@ var toggleAttirbute = function(attribute, el){
 	} else {
 		el.setAttribute(attribute, 'true');
 	};
-	el.className = el.className; // ie8
 };
 
 /* checkbox, radio */
@@ -18,9 +45,10 @@ var radio = {
 	current : undefined,
 	check : function( el ){
 		var attribute = 'checked',
-			idx = el.getAttribute('data-idx');
+			idx = dataset.get(el, 'idx');
 		if(this.current == idx ) return;
 		if(this.current){
+			el.form[el.name][this.current].setAttribute(attribute, 'false');
 			el.form[el.name][this.current].setAttribute(attribute, 'false');
 		};
 		el.setAttribute(attribute, 'true');
@@ -32,26 +60,26 @@ var radio = {
 var dropdown = {
 	expanedEl : undefined,
 	toggle : function( el ){
-		var attribute = 'aria-expanded';
+		var dataAttribute = 'expanded';
 		if( this.expanedEl && this.expanedEl != el ) {
-			this.expanedEl.setAttribute(attribute, 'false');
+			dataset.set(this.expanedEl, dataAttribute, 'false');
 		};
-		if( el.getAttribute(attribute) == 'true' ){
-			el.setAttribute(attribute, 'false');
+		if( dataset.get(el, dataAttribute) == 'true' ){
+			dataset.set(el, dataAttribute, 'false');
 		} else {
-			el.setAttribute(attribute, 'true');
+			dataset.set(el, dataAttribute, 'true');
 			window.onclick = function( evt ) {
 				dropdown.outFocus(evt);
 			};
 		};
-		// toggleAttirbute(attribute, el);
+		// toggleAttirbute(dataAttribute, el);
 		el.className = el.className; // ie8
 		this.expanedEl = el;
 	},
 	outFocus : function( evt ){
 		var target = evt.target || evt.srcElement; // Support IE6-8
-		if( target.getAttribute('data-toggle') != 'dropdown' && this.expanedEl) {
-			dropdown.expanedEl.setAttribute('aria-expanded', 'false');
+		if( dataset.get(target, 'toggle') != 'dropdown' && this.expanedEl) {
+			dataset.set(dropdown.expanedEl, 'expanded', 'false');
 			dropdown.expanedEl.className = dropdown.expanedEl.className; // ie8
 			dropdown.expanedEl = undefined;
 		};
@@ -112,94 +140,81 @@ var validation = {
 	passConfirm : function( pass1, pass2, viewEl ) {
 		viewEl = viewEl ? viewEl : pass2.nextElementSibling;
 		if( pass2.value && pass1.value != pass2.value){
-			viewEl.setAttribute('data-visible', 'true'); 
+			dataset.set(viewEl, 'visible', 'true'); 
+			// viewEl.setAttribute('data-visible', 'true'); 
 			viewEl.className = viewEl.className; // ie8
 			viewEl.textContent = 'Password and confirm password must be equal.'
 		};
 	},
 	view : function( el, type, viewEl ){
-		var dataVisible = 'data-visible';
+		var dataVisible = 'visible';
 		viewEl = viewEl ? viewEl : el.nextElementSibling;
 		if( this[type]( el.value ) ){
-			viewEl.setAttribute(dataVisible, 'false'); 
+			dataset.set(viewEl, dataVisible, 'false'); 
+			// viewEl.setAttribute(dataVisible, 'false'); 
 		} else {
-			viewEl.setAttribute(dataVisible, 'true'); 
+			dataset.set(viewEl, dataVisible, 'true'); 
+			// viewEl.setAttribute(dataVisible, 'true'); 
 		};
 		viewEl.className = viewEl.className; // ie8
 	}
 };
 
 /* Tabs */
-var tabs = function( el ){
-	var item = el.parentElement,
-		cpnt = item.parentElement.parentElement,
-		idx = item.getAttribute('data-idx');
-	cpnt.setAttribute('data-current', idx);
+var tabs = function( idName, idx ){
+	// var item = el.parentElement,
+	// 	cpnt = item.parentElement.parentElement,
+	// 	idx = dataset.get(item, 'idx');
+	var cpnt = document.getElementById(idName);
+	dataset.set(cpnt, 'current', idx);
 	cpnt.className = cpnt.className; // ie8
 };
 
-/* classList : Get IE10, polyfill */
-var modal = {
-	cpnt : document.getElementById('modal'),
-	dimmed : document.getElementById('dimmed'),
-	show : function( btn, key ){
-		var dataVisibile = 'data-visible',
-			dataAni = 'data-ani',
-			dataMiddle = 'data-middle',
-			time = 0,
-			self = this;
-		if( key == 'ani' ){
-			this.cpnt.setAttribute(dataAni, 'true');
-			this.dimmed.setAttribute(dataAni, 'true');
-			time = 50;
-		};
-		if( key == 'middle' ){
-			var wrapper = document.createElement('div');
-			wrapper.className = 'modal--middle-block';
-			window.wrap( modal.cpnt.children[0], wrapper );
-			this.cpnt.setAttribute(dataMiddle, 'true');
-		};
-		this.dimmed.setAttribute(dataVisibile, 'true');
-		this.polyfill();
-
+/* Modal*/
+var cpntModal = document.getElementById('modal'),
+	cpntDimmed = document.getElementById('dimmed'),
+	modalHideTarget = undefined;
+var modalShow = function( el ){
+	dataset.set(cpntModal, 'visible', 'true');
+	dataset.set(cpntDimmed, 'visible', 'true');
+	modalHideTarget = el;
+};
+var modalShowAnimate = function( el ){
+	// Animate pre appply
+	dataset.set(cpntModal, 'animation', 'true');
+	dataset.set(cpntDimmed, 'animation', 'true');
+	// CpntDimmed show
+	dataset.set(cpntDimmed, 'visible', 'true');
+	// After cpntDimmed animatin end, cpntModal show
+	setTimeout(function(){	
+		dataset.set(cpntModal, 'visible', 'true');
+	}, 150);
+	modalHideTarget = el;
+};
+var modalMiddle = function(){
+	dataset.set(cpntModal, 'middle', 'true');
+	var w = document.createElement('div');
+	w.className = 'modal--middle-block';
+	window.wrap( cpntModal.children[0], w );
+};
+var modalHide = function(){
+	dataset.set(cpntModal, 'visible', 'false');
+	dataset.set(cpntDimmed, 'visible', 'false');
+	modalHideTarget.focus();
+	modalHideTarget = undefined;
+	// If cpntModal vertical align middle
+	if( dataset.get(cpntModal, 'middle') == 'true' ){
+		window.unwrap( cpntModal, cpntModal.children[0] );
+		cpntModal.dataset.middle = 'fasle';
+	};
+	// If cpntModal has animation
+	if( dataset.get(cpntModal, 'animation') == 'true'){
+		// Atter cpntDimmed animatin end, animation attribute set false;
 		setTimeout(function(){
-			self.cpnt.setAttribute(dataVisibile, 'true');
-			self.polyfill();
-		}, time);
-		this.cpnt.focus();
-		this.target = btn;
-	},
-	hide : function(){
-		var dataVisibile = 'data-visible',
-			dataAni = 'data-ani',
-			dataMiddle = 'data-middle',
-			time = 0,
-			self = this;
-		if( this.cpnt.getAttribute(dataAni) == 'true' ){
-			time = 300;
-		};
-		if( this.cpnt.getAttribute(dataMiddle) == 'true' ){
-			window.unwrap( this.cpnt, this.cpnt.children[0] );
-			this.cpnt.setAttribute(dataMiddle, 'fasle');
-		};
-
-		this.cpnt.setAttribute(dataVisibile, 'false');
-		this.dimmed.setAttribute(dataVisibile, 'false');
-		this.polyfill();
-		
-		setTimeout(function(){
-			self.cpnt.setAttribute(dataAni, 'fasle');
-			self.dimmed.setAttribute(dataAni, 'false');
-			self.polyfill();
-		}, time);
-
-		this.target.focus();
-		this.target = null;
-	},
-	polyfill : function(){
-		this.dimmed.className = this.dimmed.className;
-		this.cpnt.className = this.cpnt.className;
-	}
+			dataset.set(cpntModal, 'animation', 'fasle');
+			dataset.set(cpntDimmed, 'animation', 'false');
+		}, 450);
+	};
 };
 
 /* wrap */
