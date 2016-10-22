1c1caa600bcd413e36a5449a56262524548504a9
thegi <deokim77@gmail.com>
2016-10-23 00:48:26 +0900

    tooltip focus, blur 추가

--- js/ui-base.js
+++ js/ui-base.js
@@ -240,6 +240,48 @@ var dropdown = {
 	dropdown.outFocus(evt);
 };*/
 
+/* Tooltip 
+ * @param [Element] el, element of call tolltip
+ * @param [String] place, left, right, top, bottom
+ */
+var tooltip = {
+	cpnt : document.getElementById('tooltip'),
+	placed : undefined,
+	show : function(el, place){
+		var cpnt = this.cpnt;
+		if( dataset.get(cpnt, 'visible') == 'true' && this.placed === place ) {
+			dataset.set(cpnt, 'visible', 'false');
+		} else {
+			cpnt.childNodes[1].innerText = dataset.get(el, 'tooltip') ; // Get text on data-tooltip
+			dataset.set(cpnt, 'place', place);
+			if( dataset.get(cpnt, 'visible') == 'false')
+				dataset.set(cpnt, 'visible', 'true');
+			switch(place){
+				case 'left' : 
+					cpnt.style.top = (el.offsetTop + el.offsetHeight/2 - cpnt.offsetHeight/2)  + 'px';
+					cpnt.style.left = (el.offsetLeft - cpnt.offsetWidth - 5) + 'px';
+					break;
+				case 'right' :
+					cpnt.style.top = (el.offsetTop + el.offsetHeight/2 - cpnt.offsetHeight/2)  + 'px';
+					cpnt.style.left = (el.offsetLeft + el.offsetWidth + 5) + 'px';
+					break;
+				case 'top' :
+					cpnt.style.top = (el.offsetTop - cpnt.offsetHeight - 5)  + 'px';
+					cpnt.style.left = el.offsetLeft + el.offsetWidth/2 - cpnt.offsetWidth/2 + 'px';
+					break;
+				case 'bottom' :
+					cpnt.style.top = (el.offsetTop + el.offsetHeight + 5)  + 'px';
+					cpnt.style.left = el.offsetLeft + el.offsetWidth/2 - cpnt.offsetWidth/2 + 'px';
+					break;
+			}
+		}
+		this.placed = place;
+	},
+	hide : function(){
+		dataset.set( this.cpnt, 'visible', 'false');
+	}
+};
+
 
 /* Get component's item length */
 /*var getItemLen = function(ek, tagName){
